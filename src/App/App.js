c32130f4ba83fa4e8aac583028f4087c3fbb26d0
Fabricio Augusto <fabricionaweb@gmail.com>
2019-03-26 22:24:37 +0000

    initial app design

--- src/App/App.js
+++ src/App/App.js
@@ -1,24 +1,28 @@
 import React from "react"
-import "./App.css"
+import styles from "./App.module.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="/logo.svg" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <main className={styles.main}>
+      <h1 className={styles.logo}>
+        <a href="/">
+          <img src="/logo.png" alt="Craft Beer" />
         </a>
-      </header>
-    </div>
+      </h1>
+
+      <nav className={styles.nav}>
+        <a className={styles.navItem} href="/cooler">
+          See the bar
+        </a>
+
+        <input
+          placeholder="Type to search..."
+          className={styles.searchBar}
+          type="text"
+          name="q"
+        />
+      </nav>
+    </main>
   )
 }
 
