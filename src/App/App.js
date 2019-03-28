b575b3f0bd3174d0d2ba7804bb5f03198b4c986b
Fabricio Augusto <fabricionaweb@gmail.com>
2019-03-28 23:53:35 +0000

    disable nav component

--- src/App/App.js
+++ src/App/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route } from "react-router-dom"
 import Home from "../Home/Home"
 import ViewBeer from "../ViewBeer/ViewBeer"
 import Logo from "../Logo/Logo"
-import Nav from "../Nav/Nav"
 import styles from "./App.module.css"
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
     <main className={styles.main}>
       <BrowserRouter>
         <Logo alt="Craft Beer" />
-        <Nav />
 
         <Route path="/" exact component={Home} />
         <Route path="/view/:id" component={ViewBeer} />
