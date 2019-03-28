566e649b7121a879a49e3041a918ea5f33721c3f
Fabricio Augusto <fabricionaweb@gmail.com>
2019-03-28 21:03:35 +0000

    refactor SearchResults to use hooks

--- src/SearchResults/SearchResults.js
+++ src/SearchResults/SearchResults.js
@@ -1,49 +1,21 @@
-import { Component } from "react"
+import { useState, useEffect } from "react"
 import { string, func } from "prop-types"
 import { searchByName } from "../consumer"
 
-class SearchResults extends Component {
-  state = {
+function SearchResults({ children, beerName }) {
+  const [data, setData] = useState({
     error: false,
     loading: true,
     response: {}
-  }
+  })
 
-  componentDidMount() {
-    this.fetch()
-  }
+  useEffect(() => {
+    searchByName(beerName)
+      .then(response => setData({ response }))
+      .catch(error => setData({ error }))
+  }, [beerName])
 
-  componentDidUpdate({ beerName }) {
-    if (this.props.beerName !== beerName) {
-      this.fetch()
-    }
-  }
-
-  onSuccess(response) {
-    this.setState({
-      response,
-      error: false,
-      loading: false
-    })
-  }
-
-  onFailure(error) {
-    this.setState({
-      error,
-      loading: false,
-      response: {}
-    })
-  }
-
-  fetch() {
-    searchByName(this.props.beerName)
-      .then(this.onSuccess.bind(this))
-      .catch(this.onFailure.bind(this))
-  }
-
-  render() {
-    return this.props.children(this.state)
-  }
+  return children(data)
 }
 
 SearchResults.propTypes = {
